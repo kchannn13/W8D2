36be9ef6198947449f64b0c20ce0a75e44340f0b
Kevin Chan <chankevin13@gmail.com>
2023-02-28 15:15:18 -0500

    two_sum

--- phase_1_arrays.js
+++ phase_1_arrays.js
@@ -10,3 +10,19 @@ Array.prototype.uniq = function () {
     
     return arr;
 }
+
+Array.prototype.twoSum = function () {
+    let arr = [];
+
+    for (let i = 0; i < this.length - 1; i++) {
+        if (this[i] + this[i+1] === 0){
+            arr.push([i, i+1]);
+        }
+    }
+    return arr;
+}
+
+Array.prototype.transpose = function () {
+    
+
+}
