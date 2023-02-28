8fb096d5c818355264c66e678e368d3d1e255d83
Kevin Chan <chankevin13@gmail.com>
2023-02-28 17:15:00 -0500

    finally myMap

--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -1,33 +1,22 @@
-// Array.prototype.myEach = function(callback) {
-//     for (let i = 0; i < this.length; i++) {
-//         callback(this[i]);
-//     }
-// }
-
 Array.prototype.myEach = function(callback) {
-    this.forEach(callback)
-    // this.forEach((el) => callback(el))
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
 }
-
-// const Array.prototype.myEach = (callback) => {
-//     this.forEach(callback)
-// }
-
 
 Array.prototype.myMap = function(callback) {
     let arr = [];
 
-    // for (let i = 0; i < this.length; i++) {
-    //     // num = callback(this[i])
-    //     arr.push(callback(this[i]));
-    // }
-    // return arr;
-    arr.push(this.myEach(callback));
+    const callbackToMyEach = function(el) {
+        arr.push(callback(el));
+    };
+
+    this.myEach(callbackToMyEach);
+
     return arr;
 } 
 
 function logElement(el) {
    return (el * 2);
-    // console.log(el * 2);
 }
 
